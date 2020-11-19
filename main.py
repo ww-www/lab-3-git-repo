bbe98a84323fdc71d20d24df5a5127b463987aa1
ww-www <vito@gradientts.com>
2020-11-19 14:36:02 -0500

    outputs time if named main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+"""Hello World Example."""
+# author: John Software
+
+def main():
+    print("Hello World!")
+    print("It is currently:")
+    print(now.strftime("%H:%M:%S""))
+
+if __name__ == "__main__":
+    main()
+
+# wip
